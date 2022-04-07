81afcd3f5063c9f07392ce983b474cb0bfde2d21
Alain <snowgoon88@gmail.com>
2022-04-07 11:26:35 +0200

    ADD play/pause/stop PatternAudio

--- src/pattern_audio.hpp
+++ src/pattern_audio.hpp
@@ -19,7 +19,13 @@
 class PatternAudio
 {
 public:
+  using Time = std::chrono::time_point<std::chrono::system_clock>;
+  using DurationMS = std::chrono::milliseconds;
+  enum AudioState { ready, running, paused };
+  
+public:
   PatternAudio() :
+    _state(ready),
     _start_time(std::chrono::system_clock::now())
   {
   }
@@ -27,58 +33,87 @@ public:
   {
   }
   // ******************************************************* PatternAudio::str
+  std::string str_status () const
+  {
+    std::stringstream status;
+    status << "Status: ";
+    switch (_state) {
+    case ready: 
+      status << "ready";
+      break;
+    case running:
+      status << "running";
+      break;
+    case paused:
+      status << "paused";
+      break;
+    }
+    return status.str();
+  }
+
   std::string str_dump () const
   {
     std::stringstream dump;
     dump << "Last = ";
-    std::chrono::duration<int, std::milli> delta_time_start = std::chrono::duration_cast<std::chrono::milliseconds>( _last_update - _start_time );
-    //auto as_time = std::chrono::system_clock::to_time_t(_last_update);
-    //auto millisec_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(_last_update.time_since_epoch()).count();
-    //dump << std::ctime(&as_time) << " (" << millisec_since_epoch << ")";
+    DurationMS delta_time_start = std::chrono::duration_cast<DurationMS>( _last_update - _start_time );
     dump << delta_time_start.count();
     dump << " Next = " << _time_to_next.count();
 
     dump << " id_seq = " << _id_seq;
 
+    dump << " " << str_status();
+                          
     return dump.str();
-  }
-  std::string str_time () const
-  {
-    std::stringstream time;
-    time << "txt";
-
-    return time.str();
   }
   // ****************************************************** PatternAudio::emit
   void start()
   {
-    _id_seq = 0;
-    _start_time = std::chrono::system_clock::now();
-    _time_to_next = std::chrono::milliseconds(_pattern_intervale[_id_seq]);
-    _last_update = std::chrono::system_clock::now();
-    std::cout << "Start at " << str_dump() << std::endl;
+    if (_state == ready) {
+      _id_seq = 0;
+      _start_time = std::chrono::system_clock::now();
+      _time_to_next = std::chrono::milliseconds(_pattern_intervale[_id_seq]);
+      _last_update = std::chrono::system_clock::now();
+      std::cout << "Start at " << str_dump() << std::endl;
+      _state = running;
+    }
   }
   void update()
   {
-    auto time_now = std::chrono::system_clock::now();
-    std::chrono::duration<int, std::milli> delta_time = std::chrono::duration_cast<std::chrono::milliseconds>( time_now - _last_update );
-    _time_to_next -= delta_time;
-    std::cout << str_dump() << std::endl;
+    if (_state == running) {
+      auto time_now = std::chrono::system_clock::now();
+      DurationMS delta_time = std::chrono::duration_cast<DurationMS>( time_now - _last_update );
+      _time_to_next -= delta_time;
+      std::cout << str_dump() << std::endl;
     
-    if (_time_to_next < std::chrono::duration<int, std::milli>(5)) {
-      // emit event
-      std::cout << "====> *** Emit at " << str_dump() << " ******" << std::endl;
-      _id_seq = (_id_seq + 1) % _pattern_intervale.size();
-      _time_to_next = std::chrono::milliseconds(_pattern_intervale[_id_seq]);
+      if (_time_to_next < DurationMS(5)) {
+        // emit event
+        std::cout << "====> *** Emit at " << str_dump() << " ******" << std::endl;
+        _id_seq = (_id_seq + 1) % _pattern_intervale.size();
+        _time_to_next = std::chrono::milliseconds(_pattern_intervale[_id_seq]);
+      }
+      _last_update = time_now;
     }
-    _last_update = time_now;
+    else if (_state == paused) {
+      _last_update = std::chrono::system_clock::now();
+    }
+  }
+  void pause()
+  {
+    if (_state == running) {
+      _state = paused;
+    }
+  }
+  void stop()
+  {
+    _state = ready;
   }
   // ************************************************* PatternAudio::attributs
+  AudioState _state;
   // sequence of delay in ms
   std::vector<int> _pattern_intervale = { 500, 250, 500, 500, 250 };
   uint _id_seq;
-  std::chrono::duration<int, std::milli> _time_to_next;
-  std::chrono::time_point<std::chrono::system_clock> _last_update;
-  std::chrono::time_point<std::chrono::system_clock> _start_time;
+  DurationMS _time_to_next;
+  Time _last_update;
+  Time _start_time;
 };
 #endif // PATTERN_AUDIO_HPP
