33e1252b593714451424d39cd700fb9c8769e5c2
Alain <snowgoon88@gmail.com>
2022-12-25 18:35:45 +0100

    ADD leds, only display

---
+++ src/led_display_widget.hpp
@@ -0,0 +1,185 @@
+/* -*- coding: utf-8 -*- */
+
+#ifndef LED_DISPLAY_WIDGET_HPP
+#define LED_DISPLAY_WIDGET_HPP
+
+#include <math.h>
+#include <algorithm>
+#define IMGUI_DEFINE_MATH_OPERATORS // Access to math operators
+#include "imgui_internal.h"
+
+#define W 15
+#define H 7.5
+#define v ImVec2
+
+/** 
+ * Led Display inspired by 
+ * see https://github.com/ocornut/imgui/issues/3606
+ */
+
+// ***************************************************************************
+// **************************************************************** LedDisplay
+// ***************************************************************************
+class LedDisplay
+{
+public:
+  void draw( int number )
+  {
+    ImVec2 size(320.0f, 180.0f);
+    ImGui::InvisibleButton("canvas", size);
+    ImVec2 p0 = ImGui::GetItemRectMin();
+    ImVec2 p1 = ImGui::GetItemRectMax();
+    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    draw_list->PushClipRect(p0, p1);
+
+    draw( draw_list, number,
+          p0, p1, size );
+    draw_list->PopClipRect();
+  }
+  void draw( ImDrawList *draw_list,
+             int number,
+             ImVec2 p_min, ImVec2 p_max, ImVec2 size )
+  {
+    const int max_nb_digits = 3;
+    // horizontal padding of digit = 2% of size.x
+    float pad = 0.02 * size.x;
+    // horizontal size of digit
+    float xsize_digit = size.x / max_nb_digits - pad;
+    // position of first (rightmost) digit center
+    float xpos_digit = xsize_digit * 0.5;
+    float ypos_digit = size.y * 0.5;
+
+    // actual nb of digit
+    int nb_digits = 0;
+    if (number > 0) {
+      nb_digits = std::min( max_nb_digits,
+                            static_cast<int>(trunc(log10(number)) + 1 ));
+    }
+    
+    for (int i=0; i<max_nb_digits; i++) {
+      // get digit one by one
+      int _d = number % 10;
+      number /= 10;
+
+      // to erase non-existant digits at the left
+      if (i >= nb_digits) {
+        _d = 10;
+      }
+      
+      _digit(draw_list, _d,
+             // half size of digit
+             ImVec2(xsize_digit * 0.5, ypos_digit),
+             // position of center of digit
+            ImVec2( p_max.x - xpos_digit, p_min.y + ypos_digit));
+      xpos_digit += xsize_digit + pad;
+    }
+
+  }
+  
+  // ****************************************************** LedDisplay::_digit
+private:
+  void _digit( ImDrawList *draw_list,
+               int n,
+               ImVec2 half_size, ImVec2 pos)
+  {
+    // Try to infer windowbg Color
+    const ImU32 col_bg = ImGui::GetColorU32( ImGuiCol_WindowBg );
+    // // DEBUG
+    // std::cout << "Color of " << name << " is " << col_bg4 << std::endl;
+    // std::cout << "   directly col is " << col_bg32 << std::endl;
+  
+    // TODO points for segment x0,y0 -> x1,y1
+    // Transform to the first point of segment: [x,y] * half_size + [x,y]
+    float r[7][4]={ {-1,-1,H,H},  
+                    {1,-1,-H,H},
+                    {1,0,-H,-H},
+                    {-1,1,H,-W*1.5},
+                    {-1,0,H,-H},
+                    {-1,-1,H,H},
+                    {-1,0,H,-H},
+    };
+    // looping for each segment
+    for (int i=0;i<7;i++) {
+      ImVec2 a,b;
+      // horizontal segment ? (i = 0 or 3 or 6)
+      if(i%3==0){
+        a=ImVec2( pos.x+r[i][0]*half_size.x+r[i][2],
+                  pos.y+r[i][1]*half_size.y+r[i][3]-H );
+        b=ImVec2( a.x+half_size.x*2-W,
+                  a.y+W );
+      }
+      // vertical segment ?
+      else {
+        a=ImVec2( pos.x+r[i][0]*half_size.x+r[i][2]-H,
+                  pos.y+r[i][1]*half_size.y+r[i][3] );
+        b=ImVec2( a.x+W,
+                  a.y+half_size.y-W );
+      }
+    
+      ImVec2 q = b - a;
+      float s = W * 0.6, u = s - H;
+      // // DEBUG
+      // if (i==0) {
+      //   std::cout << "----------------------" << std::endl;
+      //   std::cout << "pos.x=" << pos.x << " pos.y=" << pos.y << std::endl;
+      //   std::cout << "hs.x=" << half_size.x << " hs.y=" << half_size.y << std::endl;      
+      //   std::cout << "a.x=" << a.x << " a.y=" << a.y << std::endl;
+      //   std::cout << "b.x=" << b.x << " b.y=" << b.y << std::endl;
+      //   std::cout << "q.x=" << q.x << " q.y=" << q.y << std::endl;
+      // }
+
+      // horizontal => 6 points of the polygon (q.y is W)
+      if (q.x > q.y) {
+        ImVec2 pp[]={{ a.x + u, a.y + q.y * 0.5f},
+                     { a.x + s, a.y},
+                     { b.x - s, a.y},
+                     { b.x - u, a.y + q.y * 0.5f},
+                     { b.x - s, b.y},
+                     { a.x + s, b.y}};
+        draw_list->AddConvexPolyFilled( pp, 6,
+                                        // which color ? => on/off
+                                        //(kd[n]>>(6-i)) & 1 ? 0xffffffff : 0xff222222);
+                                        (kd[n]>>(6-i)) & 1 ? 0xffffffff : col_bg);
+      }
+      // vertical => 6 points of the polygon, q.x is W
+      else {
+        ImVec2 pp[]={
+                     {a.x+q.x*.5f,a.y+u},
+                     {b.x,a.y+s},
+                     {b.x,b.y-s},
+                     {b.x-q.x*.5f,b.y-u},
+                     {a.x,b.y-s},
+                     {a.x,a.y+s}};
+        draw_list->AddConvexPolyFilled( pp, 6,
+                                        // which color ? => on/off
+                                        (kd[n]>>(6-i)) & 1 ? 0xffffffff : col_bg);
+      }
+    }
+  }
+
+  // *************************************************** LedDisplay::attributs
+private:
+  /* 
+   Encoding of the segments of each digit.
+   order: 0 hor_up right_up right_down hor_low lef_down left_up hor_mid
+   0 = 01111110 (7E)
+   1 = 00110000 (30)
+   2 = 01101101 (6D)
+   3 = 01111001 (79)
+   4 = 00110011 (33)
+   5 = 01011011 (5B)
+   6 = 01011111 (5F)
+   7 = 01110000 (70)
+   8 = 01111111 (7F)
+   9 = 01111011 (7B)
+  10 = 00000000 (00) // to erase everything
+ */
+  const char kd[11]={0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B,
+                     0x5F, 0x70, 0x7F, 0x7B,
+                     0x00};
+  
+}; // LedDisplay class
+
+
+
+#endif // LED_DISPLAY_WIDGET_HPP
---
+++ test/skel_imgui_audio.cpp
@@ -0,0 +1,302 @@
+/* -*- coding: utf-8 -*- */
+
+
+/**
+ * Skeleton base using ImGui, miniaudio, docopt, 
+ * my custom LOGGER
+ *
+ * Exit via Ctrl-C and ESC
+ */
+
+// Parsing command line options
+#include "docopt.h"
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+#include <signal.h>          // C std lib (signal, sigaction, etc)
+
+#include <utils.hpp>        // loggers, etc
+// ***************************************************************************
+// ************************************************************* Grafik - INIT
+// ***************************************************************************
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to
+// maximize ease of testing and compatibility with old VS compilers. To link
+// with VS2010-era libraries, VS2015+ requires linking with
+// legacy_stdio_definitions.lib, which we do using this pragma. Your own project
+// should not be affected, as you are likely to link with a newer binary of GLFW
+// that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) &&                                 \
+    !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void
+glfw_error_callback(int error, const char *description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+// ***************************************************************************
+// ******************************************************************* Loggers
+// ***************************************************************************
+//#define LOG_MAIN
+#ifdef LOG_MAIN
+#  define LOGMAIN(msg) (LOG_BASE("[MAIN]", msg))
+#else
+#  define LOGMAIN(msg)
+#endif
+
+// *************************************************************** App GLOBALS
+
+bool should_exit = false;
+
+// Args
+bool _p_gui = false;
+bool _p_verb = false;
+
+// ImGui Colors
+ImVec4 hoover_color = YELLOW_COL;
+
+// ************************************************************* Clean Globals
+void clear_globals()
+{
+  LOGMAIN( "__CLEANING" );
+}
+
+// *********************************************************** Ctrl-C Callback
+void ctrlc_cbk( int s )
+{
+  LOGMAIN( "__EXIT through Ctrl-C" );
+  should_exit = true;
+}
+
+
+// ***************************************************************************
+// ******************************************************************* options
+// ***************************************************************************
+static const char _usage[] =
+R"(Drum Companion.
+
+    Usage:
+      drum_companion [-h -v -g]
+
+    Options:
+      -h --help              Show this screen
+      -v --verbose           Display some info
+      -g --gui               With GUI
+)";
+
+void setup_options( int argc, char **argv )
+{
+  std::map<std::string, docopt::value> args = docopt::docopt(_usage, 
+                                                  { argv + 1, argv + argc },
+                                                  // show help if requested
+                                                  true,
+                                                  // version string
+                                                  "Skeleton");
+
+  std::cout << "******* DocOpt arguments" << std::endl;
+  for(auto const& arg : args) {
+    std::cout << arg.first << ": " << arg.second;
+    std::cout << " type=" << type_name<decltype(arg.second)>() << std::endl;
+  }
+  std::cout << std::endl;
+  // //exit(23);
+
+  if (args["--gui"].asBool()) {
+    _p_gui = true;
+  }
+  if (args["--verbose"].asBool()) {
+    _p_verb = true;
+  }
+  //exit(22);
+
+}
+
+// ***************************************************************************
+// ******************************************************************* run_gui
+// ***************************************************************************
+int run_gui()
+{
+  // ********************************************************** GUI - creation
+  // Other GUI variables
+  bool gui_ask_end = false;
+  
+  // ******************************************************* Grafik - creation
+  // Setup window
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit())  
+    return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+  // GL ES 2.0 + GLSL 100
+  const char *glsl_version = "#version 100";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+  // GL 3.2 + GLSL 150
+  const char *glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+#else
+  // GL 3.0 + GLSL 130
+  const char *glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+
+  // only glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
+#endif
+
+  // Create window with graphics context
+  GLFWwindow *window = glfwCreateWindow(
+      600, 500, "Skeleton v1.0", NULL, NULL);
+  if (window == NULL)
+    return 1;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // Enable vsync
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad
+  // Controls
+
+  // Setup Dear ImGui style
+  // ImGui::StyleColorsDark();
+  ImGui::StyleColorsClassic();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
+  // Load Fonts
+  io.Fonts->AddFontDefault();
+  // Load font and merge to default
+  static const ImWchar icons_ranges[] = {0x23f0, 0x23ff,
+                                         0}; // static as not copied by AddFont
+  ImFontConfig config;
+  config.MergeMode = true;
+  ImFont *font_unifont = io.Fonts->AddFontFromFileTTF(
+      "ressources/unifont.ttf", 12.0f, &config, icons_ranges);
+  if (font_unifont == NULL) {
+    std::cout << "Error loading ressources/unifont.ttf" << std::endl;
+    return 1;
+  }
+
+  io.Fonts->Build();
+
+  // Try Scaling up
+  ImGui::GetStyle().ScaleAllSizes(2.0f);
+
+  // ************************************************************** Gui - Loop
+  while (!glfwWindowShouldClose(window) && !gui_ask_end) {
+    // Poll and handle events (inputs, window resize, etc.)
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // ImGUI Frame
+    {
+      // Create a window with title and append into it.
+      ImGui::Begin("Skeleton");
+
+      
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::End();
+    }
+    // Rendering
+    ImGui::Render();     int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(CLEAR_COL.x * CLEAR_COL.w, CLEAR_COL.y * CLEAR_COL.w,
+                 CLEAR_COL.z * CLEAR_COL.w, CLEAR_COL.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+
+    // test Keyboard
+    if (ImGui::IsKeyDown(526)) { // Escape
+      gui_ask_end = true;
+    }
+
+    // Now apply logic
+
+    // And deal with Play/Pause
+
+  }
+
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
+
+// ***************************************************************************
+// *********************************************************************** run
+// ***************************************************************************
+int run()
+{
+  while (! should_exit) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  }
+
+  return 0;
+}
+    
+// ***************************************************************************
+// ********************************************************************** MAIN
+// ***************************************************************************
+int main(int argc, char *argv[])
+{
+  // // To deal with Ctrl-C (Win32 and Linux)
+  signal(SIGINT, ctrlc_cbk);
+
+  // Args
+  setup_options( argc, argv );
+
+  // ************************************************************ init engines
+  LOGMAIN( "__Main Init engines");
+  if (_p_verb) {
+  }
+  
+  if (_p_gui) {
+    run_gui();
+  }
+  else {
+    run();
+  }
+  
+  // Clean up before exit
+  clear_globals();
+  
+  return 0;
+}
+// ***************************************************************************
+
